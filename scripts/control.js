d9510b3170cc21e0054ad3184c0d4a7b048cbfcd
caduovieira <cepp37@gmail.com>
2023-03-07 20:56:06 -0300

    Rating logic and thank you initial layout

--- scripts/control.js
+++ scripts/control.js
@@ -1,3 +1,16 @@
-function toggleColor() {
-    $(this).addClass('active');
-}
+let rating = 0;
+
+$('#rating div').on('click', function() {
+    if(!$(this).hasClass('active')){
+        $('#rating div').removeClass('active');
+        $(this).addClass('active');
+
+        rating = $(this).children('span:first-child').text();
+    }
+})
+
+$('#submit').on('click', function() {
+    if(rating == 0) {
+        alert("Selecione uma nota");
+    }
+})
