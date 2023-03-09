01f9fdac43bf632e5105d64292037e4b1e9c223e
caduovieira <cepp37@gmail.com>
2023-03-09 20:46:22 -0300

    Finished mobile layout

--- scripts/control.js
+++ scripts/control.js
@@ -1,16 +1,36 @@
 let rating = 0;
 
+//Função que monitora o click sobre os ratings
 $('#rating div').on('click', function() {
+    //Verifica se o botão clicado NÃO possui a classe active
     if(!$(this).hasClass('active')){
+        //Remove a classe active de todos os botões
         $('#rating div').removeClass('active');
+        //Adiciona a classe active no botão clicado
         $(this).addClass('active');
 
+        //Armazena o valor clicado na variável
         rating = $(this).children('span:first-child').text();
     }
 })
 
+//Função que monitora o click sobre o botão submit
 $('#submit').on('click', function() {
+    //Dispara o alerta se nenhuma nota foi escolhida
     if(rating == 0) {
-        alert("Selecione uma nota");
+        alert("Please, select your rating!");
+    }else {
+        //Escreve a nota armazenada na variável dentro da span
+        $('#selection span').text(rating);
+        
+        //Adiciona as classses de animação
+        $('#rating-container').addClass('animate');
+        $('#thank-you').addClass('animate');
+        
+        //Aguardar o tempo certo para os elementos surgirem na tela
+        setTimeout(function()  {
+            $('#rating-container').hide();
+            $('#thank-you').css('display', 'flex');
+        }, 150);
     }
 })
